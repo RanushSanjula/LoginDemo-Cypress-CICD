69a38bbb0702a031e60a265d89bd330443cb2874
RanushSanjula <ranush1999san@gmail.com>
2026-07-30 00:01:02 +0530

    Com New

--- cypress/e2e/invalidLogin.cy.js
+++ cypress/e2e/invalidLogin.cy.js
@@ -20,7 +20,7 @@ describe('Verify invalid login', () => {
 
 
       cy.contains(
-        'Epic sadface: Username and password do not match any user in this servide'
+        'Epic sadface: Username and password do not match any user in this service'
       )
       .should('be.visible')
 
